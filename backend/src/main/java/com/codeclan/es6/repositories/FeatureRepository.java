823ed2b44144f7187c3ec88c1dda59a1422b7e59
Alex Bazlinton <abazlinton@gmail.com>
2019-10-24 22:40:42 +0100

    remove backend

--- backend/src/main/java/com/codeclan/es6/repositories/FeatureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeclan.es6.repositories;
-
-import com.codeclan.es6.models.Feature;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeatureRepository extends JpaRepository<Feature, Long> {
-}
