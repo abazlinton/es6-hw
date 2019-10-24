38a7aef4b206cc5f2a2bfef81fe32fa907d75d86
Alex Bazlinton <abazlinton@gmail.com>
2019-10-24 17:50:08 +0100

    Vue specific data

--- frontend/vue-es6/src/assets/FeaturesData.js
+++ frontend/vue-es6/src/assets/FeaturesData.js
@@ -3,29 +3,29 @@ const FeaturesData = [
     name: "destructuring",
     description: "The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.",
     MDNLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#Object_destructuring",
-    isFullyImplemented: false,
-    implementations: "FeaturesList.js (props) - NB: Document section - 'Unpacking fields from objects passed as function parameter'"
+    isFullyImplemented: true,
+    implementations: "FeaturesList.vue"
   },
   {
     name: "class and extends",
     description: "JavaScript classes, introduced in ECMAScript 2015, are primarily syntactical sugar over JavaScript's existing prototype-based inheritance. The class syntax does not introduce a new object-oriented inheritance model to JavaScript.",
     MDNLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes",    
-    isFullyImplemented: true,
-    implementations: "App.js"
+    isFullyImplemented: false,
+    implementations: "N/A"
   },
   {
     name: "import",
     description: "The static import statement is used to import bindings which are exported by another module",
     MDNLink: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import",
-    isFullyImplemented: false,
-    implementations: "App.js, FeaturesList.js"
+    isFullyImplemented: true,
+    implementations: "App.vue, FeaturesList.vue"
   },
   {
     name: "export",
     description: "The export statement is used when creating JavaScript modules to export functions, objects, or primitive values from the module so they can be used by other programs with the import statement.",
     MDNLink: "https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export",
-    isFullyImplemented: false,
-    implementations: "FeaturesList.js, FeatureData.js"
+    isFullyImplemented: true,
+    implementations: "FeaturesList.vue, FeatureData.vue"
   }
 
 ]
